a2ec67e0a646cfc61512a4e8af5638a0f58ba884
Begginer617 <skorupskimilosz@gmail.com>
2023-10-15 20:50:39 +0200

    second commit

---
+++ src/main/java/org/example/HomePage.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+
+/* Wszystko co jest w folderze "main", a nastepnie w folderze "java" to nic innego jak klasy twrzone
+na wzor strony na ktroej elementy bedziemy automatyzować. NP stona z logowaniem.
+
+
+ */
+
+public class HomePage {
+
+
+
+
+}
