9bd4b15e0a7aa9f47076d6df3145e71192d84ff8
Begginer617 <skorupskimilosz@gmail.com>
2023-10-16 23:26:02 +0200

    fourth  commit

--- src/test/java/Login_Test.java
+++ src/test/java/Login_Test.java
@@ -1,7 +1,20 @@
 import org.testng.annotations.Test;
 
-public class Login_Test {
-   @Test
 
+public class Login_Test {
+    @Test
+    public void sumCalculation() {
+        System.out.println(5 + 1);
+    }
+
+    @Test
+    public void devideCalculation() {
+        System.out.println(6 / 2);
+    }
+
+    @Test
+    public void multiplicationCalculation() {
+        System.out.println(2 * 8);
+    }
 
 }
