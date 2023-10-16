debdc978d8707408934a32207fd97653e73feac0
Begginer617 <skorupskimilosz@gmail.com>
2023-10-17 00:40:00 +0200

    fifth  commit

--- src/test/java/Login_Test.java
+++ src/test/java/Login_Test.java
@@ -1,18 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
 
 
 public class Login_Test {
-    @Test
+    @Test(priority = 0)
     public void sumCalculation() {
         System.out.println(5 + 1);
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gamer\\OneDrive\\Dokumenty\\Chrome Driver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.onet.pl/");
     }
 
-    @Test
+
+    @Test(priority = 1)
     public void devideCalculation() {
         System.out.println(6 / 2);
     }
 
-    @Test
+    @Test(priority = 3)
     public void multiplicationCalculation() {
         System.out.println(2 * 8);
     }
